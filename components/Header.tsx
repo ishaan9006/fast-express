f4c97de764f4d2541bea5433b360b9aff3bfab33
Ishaan Khullar <ishaan.khullar06@gmail.com>
2023-03-10 13:46:56 +0530

    Added animation!

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,43 @@
 import React from "react";
+import { motion } from "framer-motion"
 
 type Props = {}
 
 export default function Header({}: Props){
     return(
         <header className="sticky top-0 flex justify-between items-center max-w-7xl mx-auto z-50 py-3">
-            <div className="flex justify-center items-end">
+            <motion.div 
+                initial={{x: -500, opacity:0, scale: 0.5}}
+                animate={{x: 0, opacity:1, scale: 1}}
+                transition={{duration:1.5}} 
+                className="flex justify-center items-end"
+            >
                 <p className="text-4xl font-bold text-[#8096FB]">F</p>
                 <p className="text-[#8096FB] text-xl font-extrabold px-1">Fast</p>
                 <p className="text-sm font-semibold">Express</p>
-            </div>
-            <div className="flex justify-around">
+            </motion.div>
+
+            <motion.div 
+                className="flex justify-around"
+                initial={{y: -500, opacity:0, scale: 0.5}}
+                animate={{y: 0, opacity:1, scale: 1}}
+                transition={{duration:1.5}} 
+            >
                 <p className="cursor-pointer px-6 text-lg font-semibold">Company</p>
                 <p className="cursor-pointer px-6 text-lg font-semibold">Our Services</p>
                 <p className="cursor-pointer px-6 text-lg font-semibold">Business Solution</p>
                 <p className="cursor-pointer px-6 text-lg font-semibold">Careers</p>
-            </div>  
+            </motion.div>  
 
-            <div className="flex justify-center items-center">
+            <motion.div 
+                className="flex justify-center items-center"
+                initial={{x: 500, opacity:0, scale: 0.5}}
+                animate={{x: 0, opacity:1, scale: 1}}
+                transition={{duration:1.5}} 
+            >
                 <p className="cursor-pointer px-4 text-lg font-semibold">Log in</p>
                 <button className="bg-[#8096FB] p-3 rounded-lg text-white font-semibold">Register</button>
-            </div>
+            </motion.div>
             
         </header>
     )
