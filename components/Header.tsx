cb2d6c0f71be66bdf5edb35651ae2a61f4978d28
Ishaan Khullar <ishaan.khullar06@gmail.com>
2023-03-10 12:39:44 +0530

    Project setup!

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {}
+
+export default function Header({}: Props){
+    return(
+        <header className="sticky top-0 bg-slate-600 h-6 z-50 max-w-7xl ">
+           
+
+        </header>
+    )
+}
